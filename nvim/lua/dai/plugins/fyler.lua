cf35bfc974779a20d2232083b909d2c47b70f5d8
Daigo Suhara <suharadaigo@gmail.com>
2026-01-30 18:10:59 +0900

    change fyler setting

--- nvim/lua/dai/plugins/fyler.lua
+++ nvim/lua/dai/plugins/fyler.lua
@@ -2,8 +2,10 @@ return {
     "A7Lavinraj/fyler.nvim",
     dependencies = { "nvim-mini/mini.icons" },
     lazy = false, -- Necessary for `default_explorer` to work properly
-    opts = {
-        vim.keymap.set("n", "<leader>e", function() require('fyler').toggle() end,
-            { desc = "Toggle Fyler View" })
-    }
+    opts = function()
+        local fyler = require('fyler')
+        vim.keymap.set("n", "<leader>e", function()
+            fyler.toggle({ dir = ".", kind = "split_right" })
+        end, { desc = "Toggle fyler View" })
+    end,
 }
