787727264bc64f2d552b62da4f8619f6826613de
Daigo Suhara <suharadaigo@gmail.com>
2026-01-31 11:02:19 +0900

    Fixed nvim boot speed

--- nvim/lua/dai/plugins/noice.lua
+++ nvim/lua/dai/plugins/noice.lua
@@ -1,33 +1,34 @@
-return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {},
-	dependencies = {
-		"MunifTranjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
-	config = function()
-		require("notify").setup({
-			background_colour = "#000000",
-		})
-		require("noice").setup({
-			messages = {
-				view = "mini",
-				view_search = "mini",
-			},
-			notify = {
-				view = "mini",
-			},
-			lsp = {
-				view = "notify",
-				progress = {
-					view = "mini",
-					enabled = true,
-					format = "lsp_progress",
-					format_done = "lsp_progress_done",
-					throttle = 1000 / 30, -- frequency to update lsp progress message
-				},
-			},
-		})
-	end,
-}
+return {}
+-- return {
+-- 	"folke/noice.nvim",
+-- 	event = "VeryLazy",
+-- 	opts = {},
+-- 	dependencies = {
+-- 		"MunifTranjim/nui.nvim",
+-- 		"rcarriga/nvim-notify",
+-- 	},
+-- 	config = function()
+-- 		require("notify").setup({
+-- 			background_colour = "#000000",
+-- 		})
+-- 		require("noice").setup({
+-- 			messages = {
+-- 				view = "mini",
+-- 				view_search = "mini",
+-- 			},
+-- 			notify = {
+-- 				view = "mini",
+-- 			},
+-- 			lsp = {
+-- 				view = "notify",
+-- 				progress = {
+-- 					view = "mini",
+-- 					enabled = true,
+-- 					format = "lsp_progress",
+-- 					format_done = "lsp_progress_done",
+-- 					throttle = 1000 / 30, -- frequency to update lsp progress message
+-- 				},
+-- 			},
+-- 		})
+-- 	end,
+-- }
