eb9da82649b1c4cae493635bc4d43bf6ef918f42
Daigo Suhara <suharadaigo@gmail.com>
2026-08-29 11:03:04 +0900

    refactor: weztermのカラースキーム，透明度など修正

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,40 +5,31 @@ config.automatically_reload_config = true
 config.font = wezterm.font_with_fallback({
 	"Cica",
 })
+config.color_scheme = "GitHub Dark"
 config.font_size = 15.0
 config.initial_cols = 170
 config.initial_rows = 55
 config.use_ime = true
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
+config.audible_bell = "Disabled"
+config.scrollback_lines = 50000
 
 ----------------------------------------------------
 -- Tab
 ----------------------------------------------------
--- タイトルバーを非表示
 config.window_decorations = "RESIZE"
--- タブバーの表示
 config.show_tabs_in_tab_bar = true
--- タブが一つの時は非表示
 config.hide_tab_bar_if_only_one_tab = true
-
--- タブバーの透過
 config.window_frame = {
 	inactive_titlebar_bg = "none",
 	active_titlebar_bg = "none",
 }
-
--- タブバーを背景色に合わせる
 config.window_background_gradient = {
 	colors = { "#000000" },
 }
-
--- タブの追加ボタンを非表示
 config.show_new_tab_button_in_tab_bar = false
--- タブの閉じるボタンを非表示
 config.show_close_tab_button_in_tabs = false
-
--- タブ同士の境界線を非表示
 config.colors = {
 	tab_bar = {
 		inactive_tab_edge = "none",
